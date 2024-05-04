1cb06d912785dc05ce48ec96147bf740a1fa7808
DucDung <dung.nd207946@gmail.com>
2024-05-04 22:39:35 +0700

    Add crud function in Movie class and Theater class

--- src/dao/MovieDao.java
+++ src/dao/MovieDao.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDao {
-    public List<Movie> getAllMovie() {
+    public List<Movie> getAllMovie() {           // Show Movie function
         List<Movie> movies = new ArrayList<>();
 
         Connection connection =JDBCConnection.getJDBCConnection();
@@ -42,7 +42,7 @@ public class MovieDao {
         return movies;
     }
 
-    public void addMovie(String title, String description,
+    public void addMovie(String title, String description, // Add movie function
                          String director, String genre, String duration,
                          int price, boolean visibility, boolean availability){
         Connection connection =JDBCConnection.getJDBCConnection();
@@ -59,6 +59,54 @@ public class MovieDao {
             preparedStatement.setBoolean(7, visibility);
             preparedStatement.setBoolean(8, availability);
 
+            int rs =preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void deleteMovieByID(int id){           // Delete movie by ID function
+        Connection connection =JDBCConnection.getJDBCConnection();
+        String sql = "DELETE FROM movie WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void hideMovieByID(int id){           // Set visibility of movie to 0 by ID
+        Connection connection =JDBCConnection.getJDBCConnection();
+        String sql = "UPDATE movie SET visibility = 0 WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editMovieByID(int id, String title, String description,                   // Edit movie function, when getting the data from UI,
+                              String director, String genre, String duration,             // pass the value into this function
+                              int price, boolean visibility, boolean availability){
+        Connection connection =JDBCConnection.getJDBCConnection();
+        String sql = "UPDATE movie " +
+                     "SET title = ?, description = ?, director = ?, genre = ?, duration = ?, price = ?, visibility = ?, availability ? " +
+                     "WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, description);
+            preparedStatement.setString(3, director);
+            preparedStatement.setString(4, genre);
+            preparedStatement.setString(5, duration);
+            preparedStatement.setInt(6, price);
+            preparedStatement.setBoolean(7, visibility);
+            preparedStatement.setBoolean(8, availability);
+            preparedStatement.setInt(9, id);
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
--- src/dao/TheaterDao.java
+++ src/dao/TheaterDao.java
@@ -1,4 +1,80 @@
 package dao;
 
+import model.Movie;
+import model.Theater;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class TheaterDao {
+    public ArrayList<Theater> getALlTheater(){                   // Get/Show all theaters
+        Connection connection =JDBCConnection.getJDBCConnection();
+        ArrayList<Theater> theaters = new ArrayList<>();
+        String sql = "SELECT * FROM theater";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()) {
+                Theater theater = new Theater();
+                theater.setName(rs.getString("name"));
+                theater.setLocation(rs.getString("location"));
+
+                theaters.add(theater);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return theaters;
+    }
+
+    public void addTheater(String name, String location){
+        Connection connection =JDBCConnection.getJDBCConnection();
+        String sql = "INSERT INTO theater(name, location) VALUES(?, ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, location);
+            int rs = preparedStatement.executeUpdate();
+            if(rs > 0){
+                System.out.println("Add theater success!");
+            }
+            else {
+                System.out.println("Nothing was inserted!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeTheaterByID(int id){
+        Connection connection =JDBCConnection.getJDBCConnection();
+        String sql = "DELETE FROM theater WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editTheaterByID(int id, String name, String location){
+        Connection connection =JDBCConnection.getJDBCConnection();
+        String sql = "UPDATE theater SET name = ?, location = ? WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, location);
+            preparedStatement.setInt(3, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
